ff75c45a4e69f5a41aced325f59aa094c125709d
Camila <camila.os95@gmail.com>
2024-09-01 14:47:38 -0400

    se agrega logo al inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -14,7 +14,7 @@
           <div class="col-sm-6">
             <h1>
             <img src="{{ config('app.sys_logo_url') }}" alt="Logo Invensys" width="40" style="vertical-align: middle; margin-right: 10px;"> 
-            Bienvenido a Invensys <i class="fas fa-store"></i>
+            Bienvenido a Invensys 
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
